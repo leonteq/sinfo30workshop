439a31fe3e2eaf37381480018cb39cd540135bad
tanguy-ltq <tanguy.bayard@leonteq.com>
2023-02-27 21:50:27 +0000

    cleanup for BasicMonteCarloPriceSimulator

--- src/main/java/com/leonteq/sinfo/montecarlo/BasicMonteCarloPriceSimulator.java
+++ src/main/java/com/leonteq/sinfo/montecarlo/BasicMonteCarloPriceSimulator.java
@@ -2,13 +2,18 @@ package com.leonteq.sinfo.montecarlo;
 
 import java.util.Random;
 
+/**
+ * this is a Monte Carlo price simulator that uses a custom random generator
+ * As it uses basic Java primitives, it is meant to be easy to port to other
+ * languages.
+ */
 public class BasicMonteCarloPriceSimulator implements MonteCarloPriceSimulator {
     // having a fixed seed allows to reproduce the same results
     // this is by design to make the exercise easier to follow
     // for something to put in production, you should use a random seed
     final long seed = 123;
     final Random random = new Random(seed);
-    private Object pyramidMutex = new Object();
+    final private Object pyramidMutex = new Object();
     private double[] customPyramid = null;
 
     @Override
